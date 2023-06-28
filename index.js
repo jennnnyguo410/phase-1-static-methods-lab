8502ad8d661f4009e184de128ad603bffff871bc
Hao Guo <jennnnyguo@gmail.com>
2023-06-28 21:26:59 +1000

    commit tree a7f8a3455528fc7304820cac08fb0b7aa90069e6

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\w\s'-]/g, '')
+  }
+
+  static titleize(string) {
+    let ignore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let arryOfWords = string.split(" ")
+
+    for (let i = 0; i < arryOfWords.length; i++) {
+      if (i == 0) {
+        result.push(this.capitalize(arryOfWords[i]))
+      }
+      else {
+        if (ignore.includes(arryOfWords[i])) {
+          result.push(arryOfWords[i])
+        } else {
+          result.push(this.capitalize(arryOfWords[i]))
+        }
+      }
+    }
+    return result.join(" ")
+  }
+
 }
